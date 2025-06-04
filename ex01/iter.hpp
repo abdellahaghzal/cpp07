ed9551f56d5290a002755e0bb8c9483816ffd769
oxo4real <abduredoxo@hotmail.com>
2025-06-04 09:14:26 +0100

    ready to push

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,15 +6,21 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 07:56:51 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/06/04 08:17:32 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/06/04 09:08:51 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template <typename T, typename func_ptr>
-void iter(T* arr, int len, func_ptr f) {
+template <typename T>
+void iter(T* arr, int len, void (*f)(T&)) {
+    for (int i = 0; i < len; i++)
+        f(arr[i]);
+}
+
+template <typename T>
+void iter(T* arr, int len, void (*f)(const T&)) {
     for (int i = 0; i < len; i++)
         f(arr[i]);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,22 +6,21 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 07:56:54 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/06/04 08:17:38 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/06/04 09:14:10 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
 
-void print(int i);
+template <typename T>
+void print(T& x) {
+    std::cout << x
+              << std::endl;
+}
 
 int main(void) {
     int arr[5] = {1 , 2, 3, 4, 5};
 
-    iter(arr, 5, print);
-}
-
-void print(int i) {
-    std::cout << i
-              << std::endl;
+    iter(arr, 5, print<int>);
 }
